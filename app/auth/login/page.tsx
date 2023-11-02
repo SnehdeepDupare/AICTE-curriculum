3461fd93b131ec026d5672c716f2f017bd4219b4
Snehdeep <duparesnehdeep@gmail.com>
2023-11-02 22:21:44 +0530

    implemented login and register forms

---
+++ app/auth/login/page.tsx
@@ -0,0 +1,11 @@
+import { LoginForm } from "@/components/auth/LoginForm";
+
+function Login() {
+  return (
+    <div className="flex flex-1 flex-col items-center justify-center py-10">
+      <LoginForm />
+    </div>
+  );
+}
+
+export default Login;
---
+++ app/auth/register/page.tsx
@@ -0,0 +1,11 @@
+import { RegisterForm } from "@/components/auth/RegisterForm";
+
+function Register() {
+  return (
+    <div className="flex flex-1 flex-col items-center justify-center py-10">
+      <RegisterForm />
+    </div>
+  );
+}
+
+export default Register;
